b529f6012a3d04082a0d7af0fe27a26bc8ce3dbc
Raniro Coelho <errecme@gmail.com>
2016-09-21 22:19:17 -0300

    add finsStart point function , add mapPoint struct

--- main.cpp
+++ main.cpp
@@ -7,21 +7,31 @@
 
 using namespace std;
 
-void readFile(string,const vector<vector<unsigned char> > &, ifstream &);//Function prototype for the reading the file function
+struct mapPoint
+{
+  int X;
+  int Y;
+  int newPaths;
+};
+
+//  TODO: Simplify declarations using template
+void readFile(string,vector<vector<unsigned char> > &, ifstream &);//Function prototype for the reading the file function
+void findStart(vector<vector<unsigned char> > &, mapPoint &); // function prototype to locate the maze spawn point tagged as 'i'. Returns an array with the coordinates: startPoint[i][j]
 
 int main () {
-
+  mapPoint startPoint;
   ifstream inFile; //Input file
   string strFileName; //File name
   vector <vector<unsigned char> > groundCluster;
 
-  strFileName = "maze.txt";
+  strFileName = "maze.txt"; // TODO: user input cin >>
   readFile(strFileName, groundCluster, inFile);
+  findStart(groundCluster, startPoint);
 
   return 0;
 }
 
-void readFile(string strFileName,const vector<vector<unsigned char> > &groundCluster, ifstream &inFile) {
+void readFile(string strFileName,vector<vector<unsigned char> > &groundCluster, ifstream &inFile) {
 
   vector<unsigned char> tempVector;
   string line;
@@ -37,9 +47,29 @@ void readFile(string strFileName,const vector<vector<unsigned char> > &groundClu
         tempVector.push_back(line[i]);
         cout << tempVector[i];
       }
+      groundCluster.push_back(tempVector);
+      cout << endl;
       tempVector.clear();
     }
     inFile.close();
   }
   else cout << "Unable to open file";
+}
+
+void findStart(vector<vector<unsigned char> > &groundCluster , mapPoint &mapPoint) {
+  int mazeLength = 5;
+  int mazeWidth = 10;
+  for (int x = 0; x < mazeLength; x++)
+  {
+    for (int y = 0; y < mazeWidth; y++)
+    {
+      if(groundCluster[x][y] == 'i') {
+        mapPoint.X = x;
+        mapPoint.Y = y;
+        cout << endl << groundCluster[x][y] << endl;
+        cout << x << endl;
+        cout << y << endl;
+      }
+    }
+  }
 }
