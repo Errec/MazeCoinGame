5743009c6090cf6eec3d3a286540e0987664d1e1
Raniro Coelho <errecme@gmail.com>
2016-09-23 00:37:01 -0300

    add map and structs

--- main.cpp
+++ main.cpp
@@ -5,37 +5,80 @@
 #include <string>
 #include <sstream>
 #include <map>
+#include <iterator>
 
 using namespace std;
 
-struct mapPoint
-{
+struct mapPoint {
   int X;
   int Y;
-  int newPaths;
+};
+
+struct mazeMap {
+  int mazeLength;
+  int mazeWidth;
+  int forksToGo;  // number of paths nodes with entries yet to be explored.
+  mapPoint startPoint;
+  vector <vector<unsigned char> > groundCluster;
+};
+
+struct explorerNotes {
+  bool foundCoin;
+  bool linkedToExitNode;
+  int timesVisitedThisBlock;
+  vector <bool> blockUnexploredEntrances; // 0 0 0 0 -> N S E W
 };
 
 //  TODO: Simplify vector declarations using template
-void readFile(int &, int &, string,vector<vector<unsigned char> > &, ifstream &);//Function prototype for the reading the file function
-void findStart(int &, int &, vector<vector<unsigned char> > &, mapPoint &); // function prototype to locate the maze spawn point tagged as 'i'.
+void readFile(string,mazeMap &, ifstream &);//Function prototype for the reading the file function
+void findStart(mazeMap &); // function prototype to locate the maze spawn point tagged as 'i'.
+bool exploreMaze(mazeMap &, map<mapPoint, explorerNotes> &);
 
 int main () {
-  mapPoint startPoint;
-  int mazeLength;
-  int mazeWidth;
+
+  mazeMap myMaze;
   ifstream inFile; //Input file
   string strFileName; //File name
-  vector <vector<unsigned char> > groundCluster;
+  map<mapPoint, explorerNotes> explorerNotebook; //  explorerNotebook[{a,b}] = c;
+  bool foundExit;
 
   strFileName = "maze.txt"; // TODO: accept user input cin >>
 
-  readFile(mazeLength, mazeWidth, strFileName, groundCluster, inFile);
-  findStart(mazeLength, mazeWidth, groundCluster, startPoint);
+  readFile(strFileName, myMaze, inFile);
+  findStart(myMaze);
+  foundExit = exploreMaze(myMaze, explorerNotebook);
 
+  if(foundExit) {
+    cout << "true" << endl;
+ // TODO
+  } else {
+    cout << "false" << endl;
+    // TODO
+  }
+
+// -------------------------------- remove this block when done --------------------------------------------------
+
+for (int i = 0; i < myMaze.mazeLength; i++)
+{
+  for (int j = 0; j < myMaze.mazeWidth; j++)
+  {
+    cout << myMaze.groundCluster[i][j];
+  }
+  cout << endl;
+}
+
+cout << "startPoint(" << myMaze.startPoint.X << "," << myMaze.startPoint.Y << ")" << endl;
+
+/*    for (auto &x: noteBook) {
+            cout << "P(" << x.first[0] << "," << x.first[1] << ")" << " => " << x.second << '\n';
+    }
+*/
+
+// -------------------------------- remove this block when done --------------------------------------------------
   return 0;
 }
 
-void readFile(int &mazeLength, int &mazeWidth,string strFileName,vector<vector<unsigned char> > &groundCluster, ifstream &inFile) {
+void readFile(string strFileName,mazeMap &myMaze, ifstream &inFile) {
 
   vector<unsigned char> tempVector;
   string line;
@@ -45,17 +88,15 @@ void readFile(int &mazeLength, int &mazeWidth,string strFileName,vector<vector<u
   {
     getline(inFile,line);
     istringstream iss(line);
-    iss >> mazeLength >> mazeWidth;
+    iss >> myMaze.mazeLength >> myMaze.mazeWidth;
 
     while(getline (inFile,line))
     {
       for(int i = 0; i < line.length() - 1; ++i)
       {
         tempVector.push_back(line[i]);
-        cout << tempVector[i];
       }
-      groundCluster.push_back(tempVector);
-      cout << endl;
+      myMaze.groundCluster.push_back(tempVector);
       tempVector.clear();
     }
     inFile.close();
@@ -63,18 +104,22 @@ void readFile(int &mazeLength, int &mazeWidth,string strFileName,vector<vector<u
   else cout << "Unable to open file";
 }
 
-void findStart(int &mazeLength, int &mazeWidth, vector<vector<unsigned char> > &groundCluster , mapPoint &mapPoint) {
-  for (int x = 0; x < mazeLength; x++)
+void findStart(mazeMap &myMaze) {
+  for (int x = 0; x < myMaze.mazeLength; x++)
   {
-    for (int y = 0; y < mazeWidth; y++)
-    {
-      if(groundCluster[x][y] == 'i') {
-        mapPoint.X = x;
-        mapPoint.Y = y;
-        cout << endl << groundCluster[x][y] << endl;
-        cout << x << endl;
-        cout << y << endl;
+    for (int y = 0; y < myMaze.mazeWidth; y++) {
+      if(myMaze.groundCluster[x][y] == 'i') {
+        myMaze.startPoint.X = x;
+        myMaze.startPoint.Y = y;
+        break;
       }
     }
   }
+}
+
+bool exploreMaze(mazeMap &myMaze, map<mapPoint, explorerNotes> &explorerNotebook) {
+  mapPoint currentPoint;
+  cout << myMaze.groundCluster[1][1] << endl;
+
+  return true;
 }
